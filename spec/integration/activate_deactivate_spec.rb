550ab859992d2564518aca04420e850a1f61f27d
Connor Savage <csavage@mdsol.com>
2015-01-30 11:45:49 -0500

    refactor activate deactivate spec

--- spec/integration/activate_deactivate_spec.rb
+++ spec/integration/activate_deactivate_spec.rb
@@ -3,14 +3,16 @@ RSpec.describe Moya do
     context 'when requesting hale json' do
       include_context 'shared drd hale context'
 
-      it 'responds idempotently to an activate call' do
-        # Create deactivated drd
+      before do
         req_body = {name: 'deactivated drd', status: 'deactivated', kind: 'standard'}.merge(can_do_hash)
         response = post(create_url, req_body)
+        @drd = parse_hale(response.body)
+      end
 
-        # Get the activate URL
-        drd = parse_hale(response.body)
-        activate_url = "#{get_transition_uri(drd, "activate")}.hale_json"
+      after { delete hale_url_for("delete", @drd) }
+
+      it 'responds idempotently to an activate call' do
+        activate_url =  hale_url_for("activate", @drd)
 
         # Activate twice.
         put activate_url
@@ -18,11 +20,8 @@ RSpec.describe Moya do
         expect(response.status).to eq(204)
 
         # Verify
-        response = get hale_url_for("self", drd), can_do_hash
+        response = get hale_url_for("self", @drd), can_do_hash
         expect(parse_hale(response.body).properties['status']).to eq('activated')
-
-        # Destroy our drd
-        delete hale_url_for("delete", drd)
       end
 
     end
@@ -32,14 +31,16 @@ RSpec.describe Moya do
     context 'when requesting hale json' do
       include_context 'shared drd hale context'
 
-      it 'responds idempotently to a deactivate call' do
-        # Create deactivated drd
+      before do
         req_body = {name: 'activated drd', status: 'activated', kind: 'standard'}.merge(can_do_hash)
         response = post(create_url, req_body)
+        @drd = parse_hale(response.body)
+      end
 
-        # Get the activate URL
-        drd = parse_hale(response.body)
-        deactivate_url = hale_url_for("deactivate", drd)
+      after { delete hale_url_for("delete", @drd) }
+
+      it 'responds idempotently to a deactivate call' do
+        deactivate_url = hale_url_for("deactivate", @drd)
 
         # Deactivate twice.
         put deactivate_url
@@ -47,11 +48,8 @@ RSpec.describe Moya do
         expect(response.status).to eq(204)
 
         # Verify
-        response = get hale_url_for("self", drd), can_do_hash
+        response = get hale_url_for("self", @drd), can_do_hash
         expect(parse_hale(response.body).properties['status']).to eq('deactivated')
-
-        # Destroy our drd
-        delete hale_url_for("delete", drd)
       end
 
     end
