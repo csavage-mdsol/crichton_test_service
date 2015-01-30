b8154c1e990de285cfe32354febd04d27165b34e
Connor Savage <csavage@mdsol.com>
2015-01-30 11:35:34 -0500

    refactor update spec

--- spec/integration/drd_update_spec.rb
+++ spec/integration/drd_update_spec.rb
@@ -14,21 +14,26 @@ RSpec.describe Moya do
         }
       }
 
-      it 'responds appropirately to an update call with all permissible attributes' do
-        # Create a drd
+      before do
         response = post create_url, drd_hash.merge(can_do_hash)
-        drd = parse_hale(response.body)
+        @drd = parse_hale(response.body)
+      end
+
+      after do
+        delete hale_url_for("delete", @drd)
+      end
+
+      it 'responds appropirately to an update call with all permissible attributes' do
+        expect(@drd.properties['name']).to eq('Pike')
 
         # Update the drd with all permissible attributes
-        expect(drd.properties['name']).to eq('Pike')
-
-        response = put hale_url_for("update", drd), update_all_properties
+        response = put hale_url_for("update", @drd), update_all_properties
         expect(response.status).to eq(303)
 
-        # Check that it is really updated
-        response = get hale_url_for("self", drd)
+        response = get hale_url_for("self", @drd)
         drd = parse_hale(response.body)
 
+        # Check that it is really updated
         expect(drd.properties.slice(*update_all_properties.keys)).to eq(update_all_properties)
       end
 
